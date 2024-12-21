cf78f30c69f95f88ec2c7419445388dea3c931e4
Brandon Bybee <bybee.brandon1@gmail.com>
2024-12-21 08:27:00 -0600

    adding all of my files and starting on my pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import App from './App.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
