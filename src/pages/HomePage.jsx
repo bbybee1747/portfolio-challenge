594df9f60a0185d650908bd39c0cbe4a9428fca6
Brandon Bybee <bybee.brandon1@gmail.com>
2024-12-21 09:10:23 -0600

    added things in my pages folder

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="container">
+        <h1>Welcome to My Portfolio</h1>
+       <div className="row">
+         <div className="col-md-12">
+              <p>
+                 This is a simple portfolio website created using React. 
+              </p>
+        </div>
+        </div>
+        </div>
+    );
+}
