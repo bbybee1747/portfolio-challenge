9553f2419d168dc56b966279b010a94da83cbf09
Brandon Bybee <bybee.brandon1@gmail.com>
2024-12-22 14:50:13 -0600

    added background image and fixed a few small issues

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,6 +6,7 @@ import weatherDashboardImage from '../assets/noaa-WHLI73X8tE0-unsplash.jpg';
 import vehicleBuilderImage from '../assets/averie-woodard-j4nBSqFf08U-unsplash.jpg';
 import readmeGeneratorImage from '../assets/ilya-pavlov-OqtafYT5kTw-unsplash.jpg';
 import studyGuideImage from '../assets/aaron-burden-LNwn_A9RGHo-unsplash.jpg';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Portfolio = () => {
     const projects = [
@@ -14,36 +15,42 @@ const Portfolio = () => {
             title: "Portfolio Application",
             image: portfolioImage,
             url: 'https://github.com/bbybee1747/portfolio-challenge.git',
+            githubImage: 'bi-github',
         },
         {
             id: 2,
             title: "Employee Tracker Application",
             image: employeeTrackerImage,
             url: 'https://github.com/bbybee1747/employee_tracker_challenge.git',
+            githubImage: 'bi-github',
         },
         {
             id: 3,
             title: "Weather Dashboard Application",
             image: weatherDashboardImage,
             url: 'https://github.com/bbybee1747/weather_api_challenge.git',
+            githubImage: 'bi-github',
         },
         {
             id: 4,
             title: "Vehicle Builder Terminal Application",
             image: vehicleBuilderImage,
             url: 'https://github.com/bbybee1747/Vehicle-Builder.git',
+            githubImage: 'bi-github',
         },
         {
             id: 5,
             title: "ReadMe Generator Terminal Application",
             image: readmeGeneratorImage,
             url: 'https://github.com/bbybee1747/ReadMe_Creation_Node_Challenge.git',
+            githubImage: 'bi-github',
         },
         {
             id: 6,
             title: "PreWork Study-Guide Website",
             image: studyGuideImage,
             url: 'https://github.com/bbybee1747/Pre-Work-Study-Guide.git',
+            githubImage: 'bi-github',
         }
     ];
 
@@ -53,11 +60,8 @@ const Portfolio = () => {
                 <div className="row row-cols-1 row-cols-md-2 g-4">
                     {projects.map((project, index) => (
                         <div key={project.id} className="col">
-                            <a
-                                href={project.url}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-decoration-none d-block position-relative"
+                            <div
+                                className="position-relative d-block"
                                 style={{
                                     overflow: 'hidden',
                                 }}
@@ -81,17 +85,28 @@ const Portfolio = () => {
                                     }}
                                 />
                                 <div
-                                    className="position-absolute top-50 start-50 translate-middle bg-white bg-opacity-75 p-2 rounded text-center"
+                                    className="position-absolute top-0 start-0 end-0 bottom-0 bg-white bg-opacity-75 d-flex flex-column align-items-center justify-content-center p-2"
                                     style={{
                                         transition: 'opacity 0.3s',
-                                        fontSize: '1.25rem',
-                                        pointerEvents: 'none',
                                         opacity: '0',
+                                        pointerEvents: 'none',
                                     }}
                                 >
-                                    {project.title}
+                                    <h5 className="mb-2">{project.title}</h5>
+                                    <a
+                                        href={project.url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className={`text-decoration-none ${project.githubImage}`}
+                                        style={{
+                                            fontSize: '2rem',
+                                            color: '#000',
+                                        }}
+                                    >
+                                        <i className={`bi ${project.githubImage}`}></i>
+                                    </a>
                                 </div>
-                            </a>
+                            </div>
                         </div>
                     ))}
                 </div>
